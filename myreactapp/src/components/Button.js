b0e317734a187c2fc900d7f29294b1af7b60db1c
Marcode777 <marcosebello7@gmail.com>
2022-02-23 06:20:29 -0500

    update

--- myreactapp/src/components/Button.js
+++ myreactapp/src/components/Button.js
@@ -5,17 +5,18 @@ class Button extends React.Component {
         super(props);
         this.state = {
             clicked: false
-        }
+        };
     }
     toggleClick(){
         console.log('clicker triggered!');
         this.setState({
-            clicked: !this.state.clicked
+            clicked: !this.state.clicked,
+            date: new Date().toLocaleString()
         })
     }
     render() {
         if(this.state.clicked) {
-            return <div>'Tuesday TWOsDAY because the date is February 22 2022, 2/22/22' <button onClick={()=> this.toggleClick()}>Button</button></div>
+            return <div>'Tuesday TWOsDAY because the date is February 22 2022, 2/22/22' <p>Current date and time is {this.state.date}</p> <button onClick={()=> this.toggleClick()}>Button</button></div>
         }
         return <button onClick={()=> this.toggleClick()}>Button</button>
     }
